e687af8d049c2b1b07451af17f7de44d8ff25fde
Almas Kilichov <itsmywaytoamerica@gmail.com>
2024-03-22 18:43:59 +0500

    feat: add recently added items to home page, but button open collection doesn't works

---
+++ src/api/users.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../consts/base-url-api";
+import { getHeaders } from "../utils/get-headers";
+
+export const usersApi = createApi({
+  reducerPath: "usersApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: getHeaders,
+  }),
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => `/users/${id}`,
+    }),
+  }),
+});
+
+export const { useGetUserQuery } = usersApi;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,23 @@
+import { Box } from "@mui/material";
+import ItemCard from "../../components/ItemCard";
+import { useGetRecentlyAddedQuery } from "../../api/items";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { data: items } = useGetRecentlyAddedQuery(null);
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        padding: 15,
+      }}
+    >
+      {items?.map((item) => (
+        <ItemCard {...item} key={item.id} />
+      ))}
+    </Box>
+  );
 };
 
 export default HomePage;
